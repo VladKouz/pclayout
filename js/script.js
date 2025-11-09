ba003bff142b903fa5c76fd1e9ed7a8f165ba26f
Vladimir Kuznetsov <kuznetmail@gmail.com>
2025-11-09 21:08:38 +0300

    Add files via upload
    Выполнена работа 1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const contents = document.querySelectorAll('.program-line__content');
+console.log(contents);
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+    const descr = elem.querySelector('.program-line__descr') 
+    // console.dir(title)
+    // title.onclick = () => {
+    //     console.log(title.nodeValue)
+    // }
+
+    title.addEventListener('click', () => {
+        console.log(title)
+        descr.classList.toggle('active')
+    })
+
+    //   title.addEventListener('click', () => {
+    //         console.log(title.nodeName)
+    //     })
+
+    //console.dir(descr.classList.remove);
+
+})
